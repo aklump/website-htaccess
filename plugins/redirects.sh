27c4f6c63ee53009433f9df561257df0348a6d8e
Aaron Klump <sourcecode@intheloftstudios.com>
2023-08-05 15:29:03 -0700

    Fix bug with redirects plugin that would not print file-level if shared was set to false.

--- plugins/redirects.sh
+++ plugins/redirects.sh
@@ -6,42 +6,50 @@ function plugin_redirects() {
 
   [[ "$output_path" ]] || return 1
 
-  # This file may elect to ignore shared redirects
-  eval $(get_config_as redirects "$config_base.redirects.inherit" true)
-  [[ "$redirects" == false ]] && return 0
+  # Load the shared/not-shared redirects to see if we need to do anything.
+  eval $(get_config_as include_shared "$config_base.redirects.inherit" true)
+  if [[ "$include_shared" == true ]]; then
+    eval $(get_config_keys_as shared_redirect_codes -a "redirects")
+  fi
+  eval $(get_config_keys_as redirect_codes -a "$config_base.redirects")
+  if [[ ${#shared_redirect_codes[@]} -eq 0 ]] && [[ ${#redirect_codes[@]} -eq 0 ]]; then
+    return
+  fi
 
   list_add_item "Using plugin: redirects"
 
-  # Handle shared redirects
-  eval $(get_config_keys_as redirect_codes -a "redirects")
-  for code in "${redirect_codes[@]}"; do
-    eval $(get_config_as redirects -a "redirects.$code")
-    for string_split__string in "${redirects[@]}"; do
-      string_split ' '
-      from=${string_split__array[0]}
-      if [[ "${from:0:1}" != '/' ]]; then
-        fail_because "All redirect from-paths must begin with forward slash; \"$from\" does not." && return 1
-      fi
-      if [[ "${from: -1}" == '/' ]]; then
-        fail_because "Redirect from-paths must not end with a forward slash; remove the slash from \"$from\"." && return 1
-      fi
-      if [[ $code -gt 299 ]] && [[ $code -lt 400 ]] && [[ ! "${string_split__array[1]}" ]]; then
-        fail_because "The redirect parameter cannot be empty for: $from" && return 1
-      fi
+  # This file may elect to ignore shared redirects
+  if [[ "$include_shared" == true ]]; then
+    # Handle shared redirects
+    for code in "${shared_redirect_codes[@]}"; do
+      eval $(get_config_as redirects -a "redirects.$code")
+      for string_split__string in "${redirects[@]}"; do
+        string_split ' '
+        from=${string_split__array[0]}
+        if [[ "${from:0:1}" != '/' ]]; then
+          fail_because "All redirect from-paths must begin with forward slash; \"$from\" does not." && return 1
+        fi
+        if [[ "${from: -1}" == '/' ]]; then
+          fail_because "Redirect from-paths must not end with a forward slash; remove the slash from \"$from\"." && return 1
+        fi
+        if [[ $code -gt 299 ]] && [[ $code -lt 400 ]] && [[ ! "${string_split__array[1]}" ]]; then
+          fail_because "The redirect parameter cannot be empty for: $from" && return 1
+        fi
 
-      from=$(_handle_special_chars "^${string_split__array[0]}/?\$")
-      if [[ "${string_split__array[1]}" ]]; then
-        to=$(_handle_special_chars "${string_split__array[1]}")
-        echo RedirectMatch $code $from $to >>"$output_path"
-      else
-        echo RedirectMatch $code $from >>"$output_path"
-      fi
+        from=$(_handle_special_chars "^${string_split__array[0]}/?\$")
+        if [[ "${string_split__array[1]}" ]]; then
+          to=$(_handle_special_chars "${string_split__array[1]}")
+          echo RedirectMatch $code $from $to >>"$output_path"
+        else
+          echo RedirectMatch $code $from >>"$output_path"
+        fi
+      done
     done
-  done
+  fi
 
   # Handle individual file redirects.
-  eval $(get_config_keys_as redirect_codes -a "$config_base.redirects")
   for code in "${redirect_codes[@]}"; do
+    [[ "$code" == "inherit" ]] && continue
     eval $(get_config_as redirects -a "$config_base.redirects.$code")
     for string_split__string in "${redirects[@]}"; do
       string_split ' '
